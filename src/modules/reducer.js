320952d0dfc11387d06120db4890bc4a3a31d457
Goya <gim.goya@gmail.com>
2023-09-07 21:58:26 +0900

    First Commit

---
+++ src/modules/reducer.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const prcSlice = createSlice({
+  name: "prac",
+  reducers: {},
+});
+
+export const {} = prcSlice.actions;
+export default prcSlice.reducer;
